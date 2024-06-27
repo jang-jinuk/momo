1b59e1d6d0c43fac63b049b9026b98f852006cdd
yyyyyjin <164327153+yyyyyjin@users.noreply.github.com>
2024-06-27 16:29:28 +0900

    0627 PJT 초기세팅 YY

---
+++ src/test/java/com/momo/momopjt/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo.momopjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
