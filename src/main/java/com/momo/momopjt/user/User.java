58bfd427c7d22b8bff49a768ecf8f3bd999e98d4
tlsdn <rlatlsdn34@gmail.com>
2024-06-27 17:50:16 +0900

    user/ userdto create by SW

---
+++ src/main/java/com/momo/momopjt/user/User.java
@@ -0,0 +1,53 @@
+package com.momo.momopjt.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user", schema = "momodb")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userNo;
+
+    private String userId;
+
+    private String userPw;
+
+    private String userEmail;
+
+    private String userNickname;
+
+    private Character userGender;
+
+    private Integer userAge;
+
+    private LocalDate userBirth;
+
+    private String  userCategory;
+
+    private String userAddress;
+
+    private String userMbti;
+
+    private Character userState;
+
+    private Character userSocial;
+
+    private String userPhoto;
+
+    private Integer userLikeNumber;
+
+    private Instant userCreateDate;
+
+    private Instant userModifyDate;
+
+    private enum userAdmin;
+
+}
---
+++ src/main/java/com/momo/momopjt/user/UserDto.java
@@ -0,0 +1,51 @@
+package com.momo.momopjt.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.time.LocalDate;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private Long userNo;
+
+    private String userId;
+
+    private String userPw;
+
+    private String userEmail;
+
+    private String userNickname;
+
+    private Character userGender;
+
+    private Integer userAge;
+
+    private LocalDate userBirth;
+
+    private String  userCategory;
+
+    private String userAddress;
+
+    private String userMbti;
+
+    private Character userState;
+
+    private Character userSocial;
+
+    private String userPhoto;
+
+    private Integer userLikeNumber;
+
+    private Instant userCreateDate;
+
+    private Instant userModifyDate;
+
+    private enum userAdmin;
+}
