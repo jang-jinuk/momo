95a1490b87515fecf1c0ec3a94b9d09c0c18015f
yyyyyjin <164327153+yyyyyjin@users.noreply.github.com>
2024-06-27 19:06:28 +0900

    0627 PJT 시큐리티 업데이트2 YY

--- src/main/java/com/momo/momopjt/global/config/CustomSecurityConfig.java
+++ src/main/java/com/momo/momopjt/global/config/CustomSecurityConfig.java
@@ -5,18 +5,12 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.web.SecurityFilterChain;
 
-
-import java.util.Arrays;
 
 @Configuration
 @Log4j2
@@ -26,8 +20,8 @@ public class CustomSecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http,
-                                           AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http
+                                           /* ,AuthenticationManagerBuilder authenticationManagerBuilder*/) throws Exception {
 
         log.info("----------------- SecFilterChain [05-31 16:04:55]-----------------");
 
