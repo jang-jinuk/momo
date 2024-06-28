429dc9146e842f1e909356a0ea145ef41a21cf09
jang-jinuk <dlsndifn98@gmail.com>
2024-06-28 15:14:24 +0900

    [club] 모임 전체 조회 기능 구현

---
+++ src/main/java/com/momo/momopjt/Photo/PhotoDTO.java
@@ -0,0 +1,12 @@
+package com.momo.momopjt.Photo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhotoDTO {
+    private String photoUuid;
+}
--- src/main/java/com/momo/momopjt/club/ClubServiceImpl.java
+++ src/main/java/com/momo/momopjt/club/ClubServiceImpl.java
@@ -2,13 +2,46 @@ package com.momo.momopjt.club;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
-@Transactional
 @RequiredArgsConstructor
 public class ClubServiceImpl implements ClubService {
+
+    private final ModelMapper modelMapper;
+    private final ClubRepository clubRepository;
+
+    @Override
+    public Long createClub(ClubDTO clubDTO) {
+        Club club = modelMapper.map(clubDTO, Club.class);
+        Long clubNo = clubRepository.save(club).getClubNo();
+        return clubNo;
+    }
+
+    @Override
+    @Transactional
+    public ClubDTO readOneClub(Long clubNo) {
+        Optional<Club> result = clubRepository.findById(clubNo);
+
+        Club club = result.orElseThrow();
+
+        ClubDTO clubDTO = modelMapper.map(club, ClubDTO.class);
+
+        return clubDTO;
+    }
+
+    @Override
+    public List<ClubDTO> readAllClub() {
+        List<Club> clubs = clubRepository.findAll();
+        List<ClubDTO> clubDTOS = modelMapper.map(clubs, List.class);
+        return clubDTOS;
+    }
+
 }
