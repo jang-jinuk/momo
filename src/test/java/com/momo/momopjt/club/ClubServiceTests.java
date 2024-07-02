e356f2819f42a52049b5dd27fe40bcd577b768a2
jang-jinuk <dlsndifn98@gmail.com>
2024-07-02 17:40:07 +0900

    test(): 0702 모임 관련 테스트 코드 수정

--- src/test/java/com/momo/momopjt/club/ClubServiceTests.java
+++ src/test/java/com/momo/momopjt/club/ClubServiceTests.java
@@ -1,7 +1,11 @@
 package com.momo.momopjt.club;
 
 import com.momo.momopjt.Photo.Photo;
+import com.momo.momopjt.Photo.PhotoDTO;
+import com.momo.momopjt.Photo.PhotoService;
+import com.momo.momopjt.user.User;
 import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,39 +17,75 @@ import java.util.List;
 @SpringBootTest
 @Log4j2
 public class ClubServiceTests {
-    @Autowired
-    private ClubService clubService;
+  @Autowired
+  private ClubService clubService;
 
-    @Test
-    public void createTest() {
-        Photo photo = new Photo();
-        photo.setPhotoUuid("테스트모임대표사진.jpg");
+  @Test
+  public void createTest() {
+    User user = new User();
+    user.setUserNo(1L);
 
-        ClubDTO clubDTO = ClubDTO.builder()
-                .clubArea("테스트 지역")
-                .clubCategory("테스트 카테고리")
-                .clubName("테스트 모임")
-                .clubContent("테스트 모임 소개")
-                .clubMax(10)
-                .clubGender('m')
-                .clubCreatedDate(Instant.now())
-                .clubMainPhoto(photo)
-                .build();
-        clubService.createClub(clubDTO);
-    }
+    PhotoDTO photoDTO = PhotoDTO.builder()
+        .photoUuid("00002test")
+        .userNo(user)
+        .photoSize(10)
+        .photoCreateDate(Instant.now())
+        .photoOriginalName("test img")
+        .photoSaveName("test save img")
+        .photoThumbnail("test thumbnail img")
+        .build();
 
-    @Test
-    @Transactional
-    public void readOneTest() {
-        ClubDTO clubDTO = clubService.readOneClub(1L);
-        log.info(clubDTO);
-    }
+    ClubDTO clubDTO = ClubDTO.builder()
+        .clubArea("테스트 지역1")
+        .clubCategory("테스트 카테고리1")
+        .clubName("테스트 모임1")
+        .clubContent("테스트 모임 소개1")
+        .clubMax(5)
+        .clubGender('m')
+        .clubCreateDate(Instant.now())
+        .build();
+    clubService.createClub(clubDTO, photoDTO);
+  }
 
-    @Test
-    @Transactional
-    public void readAllTest() {
-        List<ClubDTO> clubDTOs = clubService.readAllClub();
-        log.info(clubDTOs);
+  @Test
+  @Transactional
+  public void readOneTest() {
+    ClubDTO clubDTO = clubService.readOneClub(1L);
+    log.info(clubDTO);
+  }
 
-    }
+  @Test
+  @Transactional
+  public void readAllTest() {
+    List<ClubDTO> clubDTOs = clubService.readAllClub();
+    log.info(clubDTOs);
+  }
+
+  @Test
+  @Transactional
+  public void updateTest() {
+
+    User user = new User();
+    user.setUserNo(1L);
+
+    PhotoDTO photoDTO = PhotoDTO.builder()
+        .photoUuid("00002test")
+        .userNo(user)
+        .photoSize(10)
+        .photoCreateDate(Instant.now())
+        .photoOriginalName("test img 수정")
+        .photoSaveName("test save img 수정")
+        .photoThumbnail("test thumbnail img 수정")
+        .build();
+
+    ClubDTO clubDTO = ClubDTO.builder()
+        .clubNo(1L)
+        .clubArea("테스트 지역 수정")
+        .clubCategory("테스트 카테고리 수정")
+        .clubName("테스트 모임 수정")
+        .clubContent("테스트 모임 소개 수정")
+        .clubMax(5)
+        .build();
+    clubService.updateClub(clubDTO, photoDTO);
+  }
 }
