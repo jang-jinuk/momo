5890a7b92e71c37a914f115285cd8d1e9697f9ad
tlsdn <rlatlsdn34@gmail.com>
2024-07-04 15:01:39 +0900

    feat(): 0704 아이디 이메일 controller 추가 SW

--- src/main/java/com/momo/momopjt/user/UserController.java
+++ src/main/java/com/momo/momopjt/user/UserController.java
@@ -4,14 +4,12 @@ package com.momo.momopjt.user;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,8 +17,6 @@ import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping("/user")
@@ -88,40 +84,19 @@ public class UserController {
             session.invalidate();
             log.info("User logged out...");
         }
-
         redirectAttributes.addFlashAttribute("message", "You have been logged out successfully.");
         return "redirect:/user/login"; // 로그아웃 후 로그인 페이지로 리다이렉트
     }
 
-    @PostMapping("/submit")
-    public String handleFormSubmit(
-            @RequestParam("userId") String userId,
-            @RequestParam("userNickname") String userNickname,
-            @RequestParam("userPw") String userPw, // 비밀번호도 받지만 결과 페이지에 표시하지 않음
-            @RequestParam("userGender") char userGender,
-            @RequestParam("userBirth") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate userBirth,
-            @RequestParam("userEmail") String userEmail,
-            @RequestParam("userCategory") String userCategory,
-            @RequestParam("userAddress") String userAddress,
-            @RequestParam("userMbti") String userMbti,
-            Model model) {
-
-        // 현재 날짜를 기준으로 나이 계산
-        LocalDate currentDate = LocalDate.now();
-        int userAge = Period.between(userBirth, currentDate).getYears();
-
-        // 비밀번호는 추가하지 않음
-        model.addAttribute("userId", userId);
-        model.addAttribute("userNickname", userNickname);
-        model.addAttribute("userGender", userGender);
-        model.addAttribute("userBirth", userBirth);
-        model.addAttribute("userEmail", userEmail);
-        model.addAttribute("userCategory", userCategory);
-        model.addAttribute("userAge", userAge);
-        model.addAttribute("userAddress", userAddress);
-        model.addAttribute("userMbti", userMbti);
-
-        return "result"; // 결과를 표시할 뷰의 이름
+    //아이디 이메일 증복 처리
+    @GetMapping("/join")
+    public ResponseEntity<Boolean> checkUserIdDuplicate(@PathVariable String id) {
+        return ResponseEntity.ok(userService.checkUserIdDuplicate(id));
     }
 
+    @GetMapping("join")
+    public ResponseEntity<Boolean> checkEmailDuplicate(@PathVariable String email) {
+        return ResponseEntity.ok(userService.checkEmailDuplicate(email));
+        //서비스로
+    }
 }
