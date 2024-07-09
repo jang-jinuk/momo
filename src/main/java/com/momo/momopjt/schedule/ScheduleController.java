a0b1e201d15a2df9d229b0991f6ce3f811f47a92
jang-jinuk <dlsndifn98@gmail.com>
2024-07-09 15:58:02 +0900

    fix(): 0709 날짜 오류로 인한 ScheduleController 수정 JW
    날짜 포매팅을 통해 오류 수정 완료하였습니다.

--- src/main/java/com/momo/momopjt/schedule/ScheduleController.java
+++ src/main/java/com/momo/momopjt/schedule/ScheduleController.java
@@ -1,5 +1,7 @@
 package com.momo.momopjt.schedule;
 
+import com.momo.momopjt.club.Club;
+import com.momo.momopjt.user.User;
 import com.momo.momopjt.userandschedule.UserAndScheduleDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 @Controller
 @Log4j2
@@ -23,8 +30,23 @@ public class ScheduleController {
   }
 
   @PostMapping("/create")
-  public String scheduleCreate(ScheduleDTO scheduleDTO, UserAndScheduleDTO userAndScheduleDTO) {
+  public String scheduleCreate(ScheduleDTO scheduleDTO, Long userNo, String dateTime) {
+    User user = new User();
+    user.setUserNo(userNo);
+    Club club = new Club();
+    club.setClubNo(1L);
+    scheduleDTO.setClubNo(club);
+    UserAndScheduleDTO userAndScheduleDTO = new UserAndScheduleDTO();
+    userAndScheduleDTO.setUserNo(user);
+
+    LocalDateTime localDateTime = LocalDateTime.parse(dateTime);
+    ZonedDateTime zonedDateTime = localDateTime.atZone(ZoneId.systemDefault());
+    Instant instant = zonedDateTime.toInstant();
+    scheduleDTO.setScheduleStartDate(instant);
+    log.info("------------ [날짜/시간 포매팅 완료] ------------");
+
     Long scheduleNo= scheduleService.createSchedule(scheduleDTO,userAndScheduleDTO);
-    return "/schedule/{scheduleNo}";
+    log.info("------------ [일정 등록 완료] ------------");
+    return "/schedule/" + scheduleNo;
   }
 }
