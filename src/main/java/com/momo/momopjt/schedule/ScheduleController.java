66445b66345850714c240bce7f9aad730e947dee
jang-jinuk <dlsndifn98@gmail.com>
2024-07-09 10:56:24 +0900

    feat(): 0709 ScheduleController에 일정 생성 URL 매핑 JW

--- src/main/java/com/momo/momopjt/schedule/ScheduleController.java
+++ src/main/java/com/momo/momopjt/schedule/ScheduleController.java
@@ -1,7 +1,17 @@
 package com.momo.momopjt.schedule;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Log4j2
+@RequestMapping("/schedule")
 public class ScheduleController {
+
+  @GetMapping("/create")
+  public String scheduleCreate() {
+    return "/schedule/create";
+  }
 }
