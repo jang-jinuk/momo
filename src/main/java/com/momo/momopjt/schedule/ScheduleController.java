6a6cb9c132e6617b3736e020d5e0f7d0f7871316
jang-jinuk <dlsndifn98@gmail.com>
2024-07-09 16:28:06 +0900

    feat(): 일정 상세 페이지 이동 매핑 구현 JW

--- src/main/java/com/momo/momopjt/schedule/ScheduleController.java
+++ src/main/java/com/momo/momopjt/schedule/ScheduleController.java
@@ -6,10 +6,8 @@ import com.momo.momopjt.userandschedule.UserAndScheduleDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -24,16 +22,18 @@ public class ScheduleController {
   @Autowired
   ScheduleService scheduleService;
 
+  //일정 생성 페이지 이동
   @GetMapping("/create")
   public String scheduleCreate() {
     return "/schedule/create";
   }
 
+  //일정 생성하기
   @PostMapping("/create")
   public String scheduleCreate(ScheduleDTO scheduleDTO, Long userNo, String dateTime) {
     User user = new User();
-    user.setUserNo(userNo);
-    Club club = new Club();
+    user.setUserNo(1L); //임시 유저 정보 (유저 관련 기능 완성 후 추가할 예정)
+    Club club = new Club(); // 임시 모임 정보(수정예정)
     club.setClubNo(1L);
     scheduleDTO.setClubNo(club);
     UserAndScheduleDTO userAndScheduleDTO = new UserAndScheduleDTO();
@@ -48,5 +48,13 @@ public class ScheduleController {
     Long scheduleNo= scheduleService.createSchedule(scheduleDTO,userAndScheduleDTO);
     log.info("------------ [일정 등록 완료] ------------");
     return "/schedule/" + scheduleNo;
+  }
+
+  //일정 상세페이지 이동
+  @GetMapping("/{scheduleNo}")
+  public String scheduleView(@PathVariable("scheduleNo") Long scheduleNo, Model model) {
+    ScheduleDTO scheduleDTO = scheduleService.findSchedule(scheduleNo);
+    model.addAttribute("scheduleDTO", scheduleDTO);
+    return "/schedule/view";
   }
 }
