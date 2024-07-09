f1fd3b1e8a4f586063f27e272e60042680bcc381
jang-jinuk <dlsndifn98@gmail.com>
2024-07-09 12:20:40 +0900

    feat(): 0709 ScheduleController 일정 생성 POST 매핑JW

--- src/main/java/com/momo/momopjt/schedule/ScheduleController.java
+++ src/main/java/com/momo/momopjt/schedule/ScheduleController.java
@@ -1,8 +1,12 @@
 package com.momo.momopjt.schedule;
 
+import com.momo.momopjt.userandschedule.UserAndScheduleDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/schedule")
 public class ScheduleController {
 
+  @Autowired
+  ScheduleService scheduleService;
+
   @GetMapping("/create")
   public String scheduleCreate() {
     return "/schedule/create";
+  }
+
+  @PostMapping("/create")
+  public String scheduleCreate(ScheduleDTO scheduleDTO, UserAndScheduleDTO userAndScheduleDTO) {
+    Long scheduleNo= scheduleService.createSchedule(scheduleDTO,userAndScheduleDTO);
+    return "/schedule/{scheduleNo}";
   }
 }
