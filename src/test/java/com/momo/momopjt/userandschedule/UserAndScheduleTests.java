5258322f41ee32da971f6e458e1771d678fede41
jang-jinuk <dlsndifn98@gmail.com>
2024-07-11 14:36:03 +0900

    test(): 0711 일정 참가자 정보 조회 기능 테스트 코드 작성 JW

---
+++ src/test/java/com/momo/momopjt/userandschedule/UserAndScheduleTests.java
@@ -0,0 +1,31 @@
+package com.momo.momopjt.userandschedule;
+
+import com.momo.momopjt.schedule.Schedule;
+import com.momo.momopjt.user.UserDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+@Log4j2
+public class UserAndScheduleTests {
+
+  @Autowired
+  UserAndScheduleService userAndScheduleService;
+
+  @Test
+  public void readAllParticipantsTest() {
+    //Given
+    Schedule schedule = new Schedule();
+    schedule.setScheduleNo(1L);
+    //When
+     List<UserDTO> userDTOList= userAndScheduleService.readAllParticipants(schedule);
+    //Then
+    for (UserDTO userDTO : userDTOList) {
+      log.info(userDTO.getUserNickname());
+    }
+  }
+}
