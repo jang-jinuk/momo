d5d25a127611944154af2786688800479ea353ef
yoojeong <frozen58@naver.com>
2024-07-11 11:39:50 +0900

    feat(): 0710 구글로그인 구현
    구굴로그인 회원가입과 로그인 설정을 구현했다. 단 계정생성일 이런게디비에 저장이 안된다.
    
    BREAKING CHANGE: 구글로그인 회원가입가능
    
    Closes #1

--- src/main/java/com/momo/momopjt/user/UserRole.java
+++ src/main/java/com/momo/momopjt/user/UserRole.java
@@ -2,5 +2,17 @@ package com.momo.momopjt.user;
 
 
 public enum UserRole {
-    USER, ADMIN;
+    USER(0),
+    ADMIN(1);
+
+    private final int value;
+
+    UserRole(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }
