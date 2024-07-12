390c12314b17de604a32352c026c3b7b2d8b33f8
jang-jinuk <dlsndifn98@gmail.com>
2024-07-13 00:25:20 +0900

    style(): 0713 미사용 import 삭제 JW

--- src/main/java/com/momo/momopjt/schedule/ScheduleController.java
+++ src/main/java/com/momo/momopjt/schedule/ScheduleController.java
@@ -13,8 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpSession;
 import java.time.Instant;
 import java.time.LocalDateTime;
