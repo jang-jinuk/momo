d1fbfae682e97b25519ed8f82032f22dec2adfc4
jang-jinuk <dlsndifn98@gmail.com>
2024-07-15 12:24:59 +0900

    feat(): 0715 특정 모임에 맴버 정보를 불러오는 쿼리문 작성 JW

--- src/main/java/com/momo/momopjt/userandclub/UserAndClubRepository.java
+++ src/main/java/com/momo/momopjt/userandclub/UserAndClubRepository.java
@@ -11,7 +11,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserAndClubRepository extends JpaRepository<UserAndClub, Long> {
-  //특정 모임의 맴버(모임원) 정보를 불러오는 쿼리문
+
+  //특정 모임의 맴버 정보를 불러오는 쿼리문
+  @Query("SELECT u FROM UserAndClub u WHERE u.clubNo = :clubNo")
+  UserAndClub findMember(@Param("clubNo")Club clubNo);
+
+  //특정 모임의 모든 맴버(모임원) 정보를 불러오는 쿼리문
   @Query("SELECT u FROM UserAndClub u WHERE u.clubNo = :clubNo AND u.isLeader = :isLeader")
   List<UserAndClub> findMemberList(@Param("clubNo") Club clubNo, @Param("isLeader") Boolean isLeader);
 
