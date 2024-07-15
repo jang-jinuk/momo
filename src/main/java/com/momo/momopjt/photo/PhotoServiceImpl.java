57da5bb648dd1509f63eabbd777b277e59ab94ef
jang-jinuk <dlsndifn98@gmail.com>
2024-07-15 20:28:31 +0900

    feat(): 0715 사진 미등록 시 default 사진 저장 로직 추가 JW

--- src/main/java/com/momo/momopjt/photo/PhotoServiceImpl.java
+++ src/main/java/com/momo/momopjt/photo/PhotoServiceImpl.java
@@ -22,8 +22,15 @@ public class PhotoServiceImpl implements PhotoService {
 
   @Override
   public Photo savePhoto(PhotoDTO photoDTO) {
+    Photo photo = new Photo();
+
+    //사진을 등록하지 않으면 "default"사진 자동 저장
+    if(photoDTO.getPhotoUUID() == "") {
+      photo.setPhotoUUID("default.jpg"); //TODO 실제 디폴트 사진으로 변경필요
+      return photo;
+    }
     photoDTO.setPhotoCreateDate(Instant.now()); //등록일
-    Photo photo = modelMapper.map(photoDTO, Photo.class);
+    photo = modelMapper.map(photoDTO, Photo.class);
     photoRepository.save(photo);
     log.info(photo);
     log.info(photo.getPhotoUUID());
