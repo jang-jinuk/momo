5b19bf5bb16f576bd918e9041b8b931558834568
jang-jinuk <dlsndifn98@gmail.com>
2024-07-18 00:20:43 +0900

    feat(): 0717 해당 모임에 모든 일정 조회하는 쿼리문 작성 JW

--- src/main/java/com/momo/momopjt/schedule/ScheduleRepository.java
+++ src/main/java/com/momo/momopjt/schedule/ScheduleRepository.java
@@ -12,16 +12,22 @@ import java.util.List;
 
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-
+  //시간이 마감되지 않은 일정 조회
   @Query("SELECT s FROM Schedule s WHERE s.clubNo = :clubNo " +
       "AND s.scheduleStartDate > current_timestamp")
   List<Schedule> findOngoingSchedules(@Param("clubNo") Club clubNo);
 
+  //시간이 마감된 일정 조회
   @Query("SELECT s FROM Schedule s WHERE s.clubNo = :clubNo " +
       "AND s.scheduleStartDate < current_timestamp")
   List<Schedule> findEndSchedules(@Param("clubNo") Club clubNo);
 
+  //해당 모임에 모든 일정 삭제하는 쿼리문
   @Modifying
   @Query("DELETE FROM Schedule WHERE clubNo = :clubNo")
   void deleteAllSchedulesByClub(@Param("clubNo") Club clubNo);
+
+  //해당 모임에 모든 일정 조회
+  @Query("SELECT s FROM Schedule s WHERE s.clubNo = :clubNo")
+  List<Schedule> findSchedulesByClub(@Param("clubNo") Club clubNo);
 }
