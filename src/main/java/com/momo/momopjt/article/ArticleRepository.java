a8c896f1374d1b6629a9b17cb23f03e8b4d2fc91
jang-jinuk <dlsndifn98@gmail.com>
2024-07-22 19:46:32 +0900

    refactor(): 0722 article merge JW

--- src/main/java/com/momo/momopjt/article/ArticleRepository.java
+++ src/main/java/com/momo/momopjt/article/ArticleRepository.java
@@ -1,9 +1,12 @@
 package com.momo.momopjt.article;
 
+import com.momo.momopjt.club.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-
+  List<Article> findByClubNo( Club clubNo);
 }
