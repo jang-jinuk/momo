306a67e702d1e924b93408de6cc2bbeda5798bfe
yoojeong <frozen58@naver.com>
2024-07-22 12:07:08 +0900

    style(): article controller log 추가 JJ

--- build/generated/sources/annotationProcessor/java/main/com/momo/momopjt/photo/QPhoto.java
+++ build/generated/sources/annotationProcessor/java/main/com/momo/momopjt/photo/QPhoto.java
@@ -62,3 +62,67 @@ public class QPhoto extends EntityPathBase<Photo> {
 
 }
 
+package com.momo.momopjt.photo;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QPhoto is a Querydsl query type for Photo
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QPhoto extends EntityPathBase<Photo> {
+
+    private static final long serialVersionUID = -1732862079L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QPhoto photo = new QPhoto("photo");
+
+    public final com.momo.momopjt.article.QArticle articleNo;
+
+    public final DateTimePath<java.time.Instant> photoCreateDate = createDateTime("photoCreateDate", java.time.Instant.class);
+
+    public final ArrayPath<byte[], Byte> photoData = createArray("photoData", byte[].class);
+
+    public final StringPath photoOriginalName = createString("photoOriginalName");
+
+    public final NumberPath<Integer> photoSize = createNumber("photoSize", Integer.class);
+
+    public final StringPath photoThumbnail = createString("photoThumbnail");
+
+    public final StringPath photoUUID = createString("photoUUID");
+
+    public final com.momo.momopjt.user.QUser userNo;
+
+    public QPhoto(String variable) {
+        this(Photo.class, forVariable(variable), INITS);
+    }
+
+    public QPhoto(Path<? extends Photo> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QPhoto(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QPhoto(PathMetadata metadata, PathInits inits) {
+        this(Photo.class, metadata, inits);
+    }
+
+    public QPhoto(Class<? extends Photo> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.articleNo = inits.isInitialized("articleNo") ? new com.momo.momopjt.article.QArticle(forProperty("articleNo"), inits.get("articleNo")) : null;
+        this.userNo = inits.isInitialized("userNo") ? new com.momo.momopjt.user.QUser(forProperty("userNo")) : null;
+    }
+
+}
+
--- src/main/java/com/momo/momopjt/article/ArticleController.java
+++ src/main/java/com/momo/momopjt/article/ArticleController.java
@@ -1,5 +1,6 @@
 package com.momo.momopjt.article;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/article")
+@Log4j2
 public class ArticleController {
 
 
@@ -27,6 +29,7 @@ public class ArticleController {
   @GetMapping("/create")
   public String showCreateForm(Model model) {
     model.addAttribute("articleDTO", new ArticleDTO());
+    log.info("-------- [Article Create]-------you");
     return "article/create";
   }
 
@@ -41,6 +44,7 @@ public class ArticleController {
   @GetMapping
   public String getAllArticles(Model model) {
     List<ArticleDTO> articles = articleService.getAllArticles();
+    log.info("-------- [ getAllArticles]-------you");
     model.addAttribute("article", articles);
     return "article/list";
   }
@@ -48,6 +52,7 @@ public class ArticleController {
   // 특정 아이디의 후기글을 보여주는 페이지
   @GetMapping("/{articleNo}")
   public String getArticleById(@PathVariable Long articleNo, Model model) {
+    log.info("-------- [get ArticleById]-------you");
     ArticleDTO article = articleService.getArticleById(articleNo);
     model.addAttribute("article", article);
     return "article/detail"; // "articles/detail.html" 뷰를 반환
@@ -57,6 +62,7 @@ public class ArticleController {
   // 기존 후기글 수정 폼을 보여주는 페이지
   @GetMapping("/{articleNo}/edit")
   public String showEditForm(@PathVariable Long articleNo, Model model) {
+    log.info("-------- [get article edit]-------you");
     ArticleDTO article = articleService.getArticleById(articleNo);
     model.addAttribute("articleDTO", article);
     return "article/edit"; // "articles/edit.html" 뷰를 반환
@@ -67,6 +73,7 @@ public class ArticleController {
   // 기존 후기글을 업데이트하는 메서드
   @PostMapping("/{articleNo}/update")
   public String updateArticle(@PathVariable Long articleNo, @ModelAttribute ArticleDTO articleDTO) {
+    log.info("-------- [article update]-------you");
     articleService.updateArticle(articleNo, articleDTO);
      return "redirect:/article";
   }
@@ -75,6 +82,7 @@ public class ArticleController {
   // 기존 후기글을 삭제하는 메서드
   @PostMapping("/{articleNo}/delete")
   public String deleteArticle(@PathVariable Long articleNo) {
+    log.info("-------- [article delete]-------you");
     articleService.deleteArticle(articleNo);
     return "redirect:/article"; // 삭제 후 후기글 목록 페이지로 리디렉션
   }
