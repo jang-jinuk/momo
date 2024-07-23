96d7cf529896e2dd7abf1e72cca6fb9131c3ab74
yoojeong <frozen58@naver.com>
2024-07-23 14:52:04 +0900

    feat(): 0723 알람기능구현 JJ
    모임에 탈퇴한사람은 자동으로 알림 가게 이벤트 처리를 하였다.
    
    BREAKING CHANGE: 모임타입이벤트처리
    
    Closes #2

--- src/main/java/com/momo/momopjt/alarm/AlarmType.java
+++ src/main/java/com/momo/momopjt/alarm/AlarmType.java
@@ -3,10 +3,10 @@ package com.momo.momopjt.alarm;
 public enum AlarmType {
 
   JOIN("가입"),
-  LEAVE("탈퇴하셨습니다"),
-  LIKE("좋아요가 눌렸습니다"),
-  REPORT("신고가 접수되었습니다"),
-  KICKOUT("강퇴되었습니다");
+  LEAVE("탈퇴"),
+  LIKE("좋아요"),
+  REPORT("신고"),
+  KICKOUT("강퇴");
   private final String message;
 
   AlarmType(String message) {
