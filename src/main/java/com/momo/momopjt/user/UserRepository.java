702ba36100006663a2eeb9218efaabdf7e14d1c0
yoojeong <frozen58@naver.com>
2024-07-25 20:30:49 +0900

    feat(): 0725 회원가입프론트+유효성검사추가 JJ
    회원가입시 비밀번호 불일치여도 회원가입이 되었는데 로직을 변경하였음, 아이디,이메일,닉네임 중복검사를 넣어주었다.
    
    BREAKING CHANGE: 유효성추가
    
    Closes #1

--- src/main/java/com/momo/momopjt/user/UserRepository.java
+++ src/main/java/com/momo/momopjt/user/UserRepository.java
@@ -20,7 +20,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
   User findByUserId(String userId);
   User findByUserIdAndUserEmail(String userId, String userEmail);
   boolean existsByUserEmail(String userEmail); //UserEmail로 존재 여부 확인
-  boolean existsByUserId(String userId); // userId로 존재 여부 확인
+  boolean existsByUserId(String userId);// userId로 존재 여부 확인
+  boolean existsByUserNickname(String userNickname);
+
   @EntityGraph(attributePaths = "roleSet")
   Optional<User> findByUserEmail(String email);
 
