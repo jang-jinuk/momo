cbf20bf82b2dd4faa3e9b91b8f77e953b5d32010
yjin <yyyyyjindev@gmail.com>
2024-08-02 14:49:00 +0900

    fix(): 0802 adminController 수정

--- src/main/java/com/momo/momopjt/report/AdminController.java
+++ src/main/java/com/momo/momopjt/report/AdminController.java
@@ -1,5 +1,7 @@
 package com.momo.momopjt.report;
 
+import com.momo.momopjt.user.UserDTO;
+import com.momo.momopjt.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,10 @@ public class AdminController {
   @Autowired
   private ReportService reportService;
 
+  @Autowired
+  private UserService userService;
+
+
 
   //조회와 페이징 검색
   @GetMapping("/manage-report")
@@ -33,6 +39,7 @@ public class AdminController {
                              @RequestParam(value = "page", defaultValue = "1") int page,
                              @RequestParam(value = "query", defaultValue = "") String query) {
     log.info(".......... [GET /manage-report]..........KSW");
+
     // 검색어를 사용하여 리포트 조회
     List<ReportDTO> findReports;
 
@@ -64,7 +71,8 @@ public class AdminController {
     model.addAttribute("query", query); // 검색어를 모델에 추가
     model.addAttribute("startPage", startPage);
     model.addAttribute("endPage", endPage);
-    log.trace("...... [AdminRtportController/manage-report/ END]..........KSW");
+
+    log.trace("...... [AdminController /manage-report/ END]..........KSW");
     return "admin/manage-report";
   }
 
@@ -108,4 +116,59 @@ public class AdminController {
     String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8);
     return "redirect:/admin/manage-report?page=" + currentPage + "&query=" + encodedQuery;
   }
+
+  @GetMapping("/manage-user")
+  public String manageUserGet(Model model,
+                              @RequestParam(value = "page", defaultValue = "1") int page,
+                              @RequestParam(value = "query", defaultValue = "") String query) {
+    log.info("GET /manage-user");
+
+    List<UserDTO> findUsers;
+    if (query.isEmpty()) {
+      findUsers = userService.readALLUsers();
+    } else {
+      findUsers = userService.searchUsers(query);
+    }
+
+    int totalUsers = findUsers.size();
+    int pageSize = 10;
+    int lastPage = (totalUsers + pageSize - 1) / pageSize;
+
+    int fromIndex = (page - 1) * pageSize;
+    int toIndex = Math.min(fromIndex + pageSize, totalUsers);
+    List<UserDTO> users = findUsers.subList(fromIndex, toIndex);
+
+    int pageGroupSize = 10;
+    int currentGroup = (page - 1) / pageGroupSize;
+    int startPage = currentGroup * pageGroupSize + 1;
+    int endPage = Math.min(startPage + pageGroupSize - 1, lastPage);
+
+    model.addAttribute("UserDTO", users);
+    model.addAttribute("page", page);
+    model.addAttribute("lastPage", lastPage);
+    model.addAttribute("query", query);
+    model.addAttribute("startPage", startPage);
+    model.addAttribute("endPage", endPage);
+
+    log.trace("AdminController /manage-user END");
+    return "admin/manage-User";
+  }
+
+  @PostMapping("/change-userRole")
+  public String changeUserrolePost(@RequestParam("userNo") Long userNo,
+                                   @RequestParam("currentPage") int currentPage,
+                                   @RequestParam("query") String query) throws UnsupportedEncodingException {
+    log.info("...... [POST /change-userRole]..........KSW");
+
+    UserDTO userDTO = new UserDTO(); // 타입에 맞게 객체를 생성하여
+    userDTO.setUserNo(userNo);
+
+    userService.chageRoleUser(userDTO); //기능으로 넘겨준다
+    log.info("...... [권한부여]..........KSW");
+    log.info("...... [{}]..........KSW",query);
+    String encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8); //쿼리를 인코더에 담아준다
+
+    return "redirect:/admin/manage-user?page=" + currentPage + "&query=" + encodedQuery;
+  }
+
 }
