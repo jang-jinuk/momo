777630fcec1b1f56dfade6ed27757ad7ad98f788
yjin <yyyyyjindev@gmail.com>
2024-08-03 20:12:52 +0900

    fix(): 0803 user update 수정 YY

--- src/main/java/com/momo/momopjt/home/HomeController.java
+++ src/main/java/com/momo/momopjt/home/HomeController.java
@@ -61,7 +61,7 @@ public class HomeController {
 
     //프사 추가 (비활성화) aop?
 
-    if(user != null ) {
+    if (user != null) {
       String userPhoto = photoService.getPhoto(user.getUserPhoto()).toString();
       model.addAttribute("userPhoto", userPhoto);
     }
@@ -72,8 +72,7 @@ public class HomeController {
     model.addAttribute("newsList", newsList);
 
 
-      return "home"; // 홈 페이지의 Thymeleaf 템플릿 이름
-    }
-    return "home";
+    return "home"; // 홈 페이지의 Thymeleaf 템플릿 이름
   }
 }
+
