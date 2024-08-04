cd23583fbb72903cca32c58d611a0f5c1819f3c5
yoojeong <frozen58@naver.com>
2024-08-04 22:07:45 +0900

    feat(): report 프론트2JJ

--- src/main/java/com/momo/momopjt/user/UserDTO.java
+++ src/main/java/com/momo/momopjt/user/UserDTO.java
@@ -14,50 +14,29 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class UserDTO {
-
-
     private Long userNo;
-
     private String userId;
-
     private String userPw;
-
     @Email
     private String userEmail;
-
     private String userNickname;
-
     private Character userGender;
-
     private Integer userAge;
-
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate userBirth;
-
     private String  userCategory;
-
     private String userAddress;
-
     private String userMBTI;
-
     private Character userState = '0';
-
     private Character userSocial='M';
-
     private String userPhoto;
     private String userPhotoStr;
-
     private Integer userLikeNumber= 0; // 기본값 설정
-
     private Instant userCreateDate;
-
     private Instant userModifyDate;
-
     private String confirmUserPw;
-
     public String getUserGenderAsString() {
         return userGender != null ? userGender.toString() : "";
     }
-
     public UserRole userRole;
 }
